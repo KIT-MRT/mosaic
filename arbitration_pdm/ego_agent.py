9d664001da6d569286fff4d2526747b88d942b99
Nick Le Large <nick.lelarge@kit.edu>
2025-12-11 16:02:59 +0100

    Include emergency stop behavior

--- arbitration_pdm/ego_agent.py
+++ arbitration_pdm/ego_agent.py
@@ -1,6 +1,6 @@
 from typing import Optional, Type
 
-from arbitration_graphs import CostArbitrator
+from arbitration_graphs import CostArbitrator, PriorityArbitrator
 from nuplan.planning.simulation.observation.observation_type import (
     DetectionsTracks,
     Observation,
@@ -13,10 +13,12 @@ from nuplan.planning.simulation.planner.abstract_planner import (
 from nuplan.planning.simulation.trajectory.abstract_trajectory import AbstractTrajectory
 from nuplan.planning.simulation.trajectory.trajectory_sampling import TrajectorySampling
 
+from arbitration_pdm.behavior.emergency_stop_behavior import EmergencyStopBehavior
 from arbitration_pdm.behavior.pdm_closed import PDMClosedBehavior
 from arbitration_pdm.behavior.pdm_open import PDMOpenBehavior
 from arbitration_pdm.common.environment_model import EnvironmentModel
 from arbitration_pdm.cost_estimator import TrajectoryCostEstimator
+from arbitration_pdm.planner.emergency_stop_planner import EmergencyStopPlanner
 from arbitration_pdm.verifier import TrajectoryVerifier
 
 
@@ -26,6 +28,14 @@ class EgoAgent(AbstractPlanner):
     class Parameters:
         trajectory_sampling: TrajectorySampling = TrajectorySampling(
             time_horizon=8.0, interval_length=0.2
+        )
+        emergency_stop_behavior: EmergencyStopBehavior.Parameters = (
+            EmergencyStopBehavior.Parameters(
+                emergency_brake_planner=EmergencyStopPlanner.Parameters(
+                    trajectory_sampling=trajectory_sampling,
+                    target_acceleration=-5.0,
+                )
+            )
         )
 
     def __init__(
@@ -44,6 +54,9 @@ class EgoAgent(AbstractPlanner):
     def initialize_arbitration_graph(self) -> None:
         self.pdm_closed_behavior = PDMClosedBehavior()
         self.pdm_open_behavior = PDMOpenBehavior()
+        self.emergency_stop_behavior: EmergencyStopBehavior = EmergencyStopBehavior(
+            self.parameters.emergency_stop_behavior
+        )
 
         self.verifier = TrajectoryVerifier()
         self.cost_estimator = TrajectoryCostEstimator()
@@ -60,6 +73,17 @@ class EgoAgent(AbstractPlanner):
             self.pdm_open_behavior,
             CostArbitrator.Option.Flags.NO_FLAGS,
             self.cost_estimator,
+        )
+
+        self.root_arbitrator: PriorityArbitrator = PriorityArbitrator(
+            "RootArbitrator", self.verifier
+        )
+
+        self.root_arbitrator.add_option(
+            self.cost_arbitrator, int(PriorityArbitrator.Option.Flags.NO_FLAGS)
+        )
+        self.root_arbitrator.add_option(
+            self.emergency_stop_behavior, int(PriorityArbitrator.Option.Flags.FALLBACK)
         )
 
     def initialize(self, initialization: PlannerInitialization) -> None:
@@ -81,7 +105,7 @@ class EgoAgent(AbstractPlanner):
         self.environment_model.update(current_input)
 
         current_time = self.environment_model.current_time_delta
-        command = self.cost_arbitrator.get_command(current_time, self.environment_model)
+        command = self.root_arbitrator.get_command(current_time, self.environment_model)
 
         return command.trajectory
 
@@ -90,6 +114,7 @@ class EgoAgent(AbstractPlanner):
         Custom getstate to avoid pickling the arbitration graph (C++ object).
         """
         state = self.__dict__.copy()
+        state["root_arbitrator"] = None
         state["cost_arbitrator"] = None
         return state
 
