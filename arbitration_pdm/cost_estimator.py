8b5cb35d96519f294ed02aa6dba907d4e3832dfc
Nick Le Large <nick.lelarge@kit.edu>
2025-12-17 13:06:35 +0100

    Remove old trajectory evaluator

--- arbitration_pdm/cost_estimator.py
+++ arbitration_pdm/cost_estimator.py
@@ -6,16 +6,12 @@ from typing_extensions import override
 
 from arbitration_pdm.common.command import Command
 from arbitration_pdm.common.environment_model import EnvironmentModel
-from arbitration_pdm.trajectory_evaluator import ImprovedTrajectoryEvaluator
 
 
 @final
 class TrajectoryCostEstimator(CostEstimator):
     def __init__(self):
         super().__init__()
-        self.trajectory_evaluator: ImprovedTrajectoryEvaluator = (
-            ImprovedTrajectoryEvaluator()
-        )
 
     @override
     def estimate_cost(
@@ -25,37 +21,16 @@ class TrajectoryCostEstimator(CostEstimator):
         command: Command,
         is_active: bool,
     ) -> float:
+        print(
+            f"Estimating cost for {command.name} with a trajectory of length {len(command.ego_states())}"
+        )
         score = environment_model.scorer.score(command.trajectory)
-        # score should only contain an ndarray containing a single score since we are scoring a single trajectory
         if score.shape != (1,):
             raise ValueError(
                 f"Expected score shape (1,), got {score.shape} when scoring trajectory."
             )
+        print(f"Raw PDM score: {score[0]}")
         return -score[0]
-
-        # TODO: Make configurable
-        SWITCHING_PENALTY = 20.0
-
-        ego_state = environment_model.ego_state
-        agents = environment_model.agents
-
-        score, _details = self.trajectory_evaluator.evaluate_trajectory_detailed(
-            ego_state=ego_state,
-            surrounding_objects=agents,
-            trajectory=command.ego_states(),
-        )
-
-        # TODO: Try replicating the original decision logic more closely
-
-        # Lower cost = better trajectory
-        cost = -score.total_score
-
-        # Penalize switching: if this trajectory is NOT currently active,
-        # make it more expensive to choose
-        if not is_active:
-            cost += SWITCHING_PENALTY
-
-        return cost
 
     def __getstate__(self) -> dict[str, object]:
         """
